f19ed0372aa4caf85a88c74729f1575b9c87b8f2
Ariom Pimenta <ariompf@gmail.com>
2024-04-26 07:42:41 -0300

    melhorias do codigo

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-function tocaSomPom() {
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listeDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listeDeTeclas.length) {
+
+    const tecla = listeDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+
+    //console.log(contador);
+}
+
+
+/* function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom ;
