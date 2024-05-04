87bd04b15386f473b2e20712d33662dceccb5326
Ariom Pimenta <ariompf@gmail.com>
2024-05-04 06:51:55 -0300

    atualizar function tocaSom

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
 }
 
 const listeDeTeclas = document.querySelectorAll('.tecla');
@@ -18,6 +24,17 @@ for (let contador = 0;contador < listeDeTeclas.length; contador++) {
     }
 
     //console.log(contador);
+    tecla.onkeydown = function(evento) {
+/*         console.log(evento.code == 'Space') */
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
 
 
